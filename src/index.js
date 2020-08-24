b5d0a987365e3d2b093705cb5d1f591443ef0c7d
socratone <gim2origin@gmail.com>
2020-08-25 01:22:17 +0900

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SlideBanner from './components/slideBanner';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SlideBanner />
   </React.StrictMode>,
   document.getElementById('root')
 );
