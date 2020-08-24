cfb7810f12b8956db5087a2c09cab29e0a5009b2
socratone <gim2origin@gmail.com>
2020-08-25 01:32:48 +0900

    wrapper로 쓸 App.jsx 생성

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SlideBanner from './components/slideBanner'
+
+const App = () => {
+  return (
+    <header>
+      <SlideBanner />
+    </header>
+  );
+}
+
+export default App;
--- src/components/slideBanner.jsx
+++ src/components/slideBanner.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const SlideBanner = () => {
-    return ( <div>hello world</div> );
+	return (
+		<section>
+			hello world
+		</section>
+	);
 }
- 
+
 export default SlideBanner;
