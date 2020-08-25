243572766096c83accc9a54f39ed6cac6cd32bb5
socratone <gim2origin@gmail.com>
2020-08-25 15:20:08 +0900

    scss 합치기

--- src/components/slideBanner.jsx
+++ src/components/slideBanner.jsx
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react';
 import SlideBannerArticle from './slideBannerArticle';
 import slideBannerData from '../lib/slideBannerData';
 import './slideBanner.scss';
-import './constantRatio.scss';
 
 const articleWrapper = React.createRef();
 
