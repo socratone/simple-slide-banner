cf6d51d1c57f1da1db482b80efd5f4759bb7b666
socratone <gim2origin@gmail.com>
2020-08-25 15:43:53 +0900

    fakeData 수정

--- src/lib/slideBannerData.js
+++ src/lib/slideBannerData.js
@@ -1,10 +1,40 @@
 const slideBanner = [
-  { id: 1, title: '첫 번째 제목', subTitle: '파격 할인', image: 'https://picsum.photos/300/200' },
-  { id: 2, title: '두 번째 제목', subTitle: '파격 할인', image: 'https://picsum.photos/300/200' },
-  { id: 3, title: '세 번째 제목', subTitle: '파격 할인', image: 'https://picsum.photos/300/200' },
-  { id: 4, title: '네 번째 제목', subTitle: '파격 할인', image: 'https://picsum.photos/300/200' },
-  { id: 5, title: '다섯 번째 제목', subTitle: '파격 할인', image: 'https://picsum.photos/300/200' },
-  { id: 6, title: '여섯 번째 제목', subTitle: '파격 할인', image: 'https://picsum.photos/300/200' },
+  {
+    id: 1,
+    title: '첫 번째 제목',
+    subTitle: '파격 할인',
+    image: 'https://picsum.photos/300/200',
+  },
+  {
+    id: 2,
+    title: '두 번째 제목',
+    subTitle: '파격 할인',
+    image: 'https://picsum.photos/300/201',
+  },
+  {
+    id: 3,
+    title: '세 번째 제목',
+    subTitle: '파격 할인',
+    image: 'https://picsum.photos/300/202',
+  },
+  {
+    id: 4,
+    title: '네 번째 제목',
+    subTitle: '파격 할인',
+    image: 'https://picsum.photos/300/203',
+  },
+  {
+    id: 5,
+    title: '다섯 번째 제목',
+    subTitle: '파격 할인',
+    image: 'https://picsum.photos/300/204',
+  },
+  {
+    id: 6,
+    title: '여섯 번째 제목',
+    subTitle: '파격 할인',
+    image: 'https://picsum.photos/300/205',
+  },
 ];
 
 export default slideBanner;
