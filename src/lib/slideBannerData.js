b4e6b5829968cb983c2ffeb978f2e0b7abb28358
socratone <gim2origin@gmail.com>
2020-08-26 11:39:37 +0900

    배경으로 들어가는 그림 파일 수정

--- src/lib/slideBannerData.js
+++ src/lib/slideBannerData.js
@@ -3,37 +3,37 @@ const slideBanner = [
     id: 1,
     title: '첫 번째 제목',
     subTitle: '파격 할인',
-    image: 'https://picsum.photos/300/200',
+    image: 'https://picsum.photos/1000/400',
   },
   {
     id: 2,
     title: '두 번째 제목',
     subTitle: '파격 할인',
-    image: 'https://picsum.photos/300/201',
+    image: 'https://picsum.photos/1000/401',
   },
   {
     id: 3,
     title: '세 번째 제목',
     subTitle: '파격 할인',
-    image: 'https://picsum.photos/300/202',
+    image: 'https://picsum.photos/1000/402',
   },
   {
     id: 4,
     title: '네 번째 제목',
     subTitle: '파격 할인',
-    image: 'https://picsum.photos/300/203',
+    image: 'https://picsum.photos/1000/403',
   },
   {
     id: 5,
     title: '다섯 번째 제목',
     subTitle: '파격 할인',
-    image: 'https://picsum.photos/300/204',
+    image: 'https://picsum.photos/1000/404',
   },
   {
     id: 6,
     title: '여섯 번째 제목',
     subTitle: '파격 할인',
-    image: 'https://picsum.photos/300/205',
+    image: 'https://picsum.photos/1000/405',
   },
 ];
 
